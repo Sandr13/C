eefa141cccd02d5d4381ec769b1a7295bedf2721
Sandr13 <55847006+Sandr13@users.noreply.github.com>
2022-03-31 21:24:56 +0300

    Add files via upload

---
+++ hw1_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int years, days;
+
+    printf("Enter years: ");
+    scanf("%d", &years);
+
+    days = 365 * years;
+
+    printf("%d years: %d days.", years, days);
+
+}
---
+++ hw1_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    int toes = 10;
+
+    printf("toes: %d, double toes: %d, squared toes: %d.", toes, toes * 2, toes * toes);
+
+}
